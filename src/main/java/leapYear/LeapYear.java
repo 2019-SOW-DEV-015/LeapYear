85f01543516c5a1b167442409713219475fccd53
2019-SOW-DEV-015 <amk.ganpat@gmail.com>
2019-05-08 01:20:03 +0200

    Refactored by adding constant in production code for the year 1582 (Gregorian calendar starts from)

--- src/main/java/leapYear/LeapYear.java
+++ src/main/java/leapYear/LeapYear.java
@@ -1,22 +1,28 @@
 package leapYear;
 
 public class LeapYear {
+
+    public static final int GREGORIAN_CALENDAR_INTRODUCED_YEAR = 1582;
+    public static final int FOUR = 4;
+    public static final int HUNDRED = 100;
+    public static final int FOUR_HUNDRED = 400;
+
     public boolean isLeapYear(int year){
-        if(year<1582){
+        if(year< GREGORIAN_CALENDAR_INTRODUCED_YEAR){
             return false;
         }else
             return (isMultipleOf400(year) || (isMultipleOf4(year) && !isMultipleOf100(year)));
     }
 
     private boolean isMultipleOf4(int year) {
-        return year%4 == 0;
+        return year%FOUR == 0;
     }
 
     private boolean isMultipleOf100(int year) {
-        return year%100 == 0;
+        return year%HUNDRED == 0;
     }
 
     private boolean isMultipleOf400(int year) {
-        return year%400 == 0;
+        return year%FOUR_HUNDRED == 0;
     }
 }
