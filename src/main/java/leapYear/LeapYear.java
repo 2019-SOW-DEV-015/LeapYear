ce98b3b781a15f12bc1b69773b1c5cd42287db98
2019-SOW-DEV-015 <amk.ganpat@gmail.com>
2019-05-08 01:00:01 +0200

    Made production code more readable

--- src/main/java/leapYear/LeapYear.java
+++ src/main/java/leapYear/LeapYear.java
@@ -2,6 +2,18 @@ package leapYear;
 
 public class LeapYear {
     public boolean isLeapYear(int year){
-        return (year%400 == 0 || year%4 == 0 && year%100 != 0);
+        return (isMultipleOf400(year) || (isMultipleOf4(year) && !isMultipleOf100(year)));
+    }
+
+    private boolean isMultipleOf4(int year) {
+        return year%4 == 0;
+    }
+
+    private boolean isMultipleOf100(int year) {
+        return year%100 == 0;
+    }
+
+    private boolean isMultipleOf400(int year) {
+        return year%400 == 0;
     }
 }
